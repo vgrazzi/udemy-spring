d151cad6e17d8c3356c1ae917188d94e20bd53dc
Victor Grazziani <victor.grazziani@outlook.com>
2023-01-05 22:26:59 -0300

    Exercise 2
    Implemented random fortune

--- src/com/luv2code/springdemo/HappyFortuneService.java
+++ src/com/luv2code/springdemo/HappyFortuneService.java
@@ -1,11 +1,18 @@
 package com.luv2code.springdemo;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class HappyFortuneService implements FortuneService {
-
-	public String getFortune() {
-		
-		return "Today is your lucky day!";
-		
+				
+	public static String getRandomFortune() {
+		String[] arr = {"Today is your lucky day!", "You'll win your next match!", "You'll be the MVP!"};
+	    int randIdx = ThreadLocalRandom.current().nextInt(arr.length);
+	    String randomFor = arr[randIdx];
+	    return randomFor;
 	}
 
+		@Override
+	public String getFortune() {
+		return getRandomFortune();
+	}
 }
+
